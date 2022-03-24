37ede0c4740158866f5d24e4e943c343058426fb
su-luq <msubiw@gmail.com>
2022-03-24 21:03:30 +0100

    update

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='d-flex flex-column justify-content.between container fullscreen'>
+      
+      <header>Cabecera</header>
+      
+      <main>
+        principal
+      </main>
+      
+      <footer>Pie</footer>
+    </div>
   )
 }
