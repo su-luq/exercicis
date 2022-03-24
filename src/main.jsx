287c9a10f7eec120161ad13697d8968e8e6b2175
su-luq <msubiw@gmail.com>
2022-03-24 20:55:42 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
+import Home from './componentes/Home'
+import Clientes from './componentes/Clientes'
+import Facturas from './componentes/Facturas'
+import Productos from './componentes/Productos'
 
 ReactDOM.render(
   <BrowserRouter>
