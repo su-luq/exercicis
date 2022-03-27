05c19db1d094ff53161cd297b5c64d17a77e90f7
Marceli Subirana <msubiw@gmail.com>
2022-03-27 18:39:12 +0200

    Update

--- src/componentes/productos.jsx
+++ src/componentes/productos.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+// afegim yarn add axios
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
-export default function Productos() {
+const URL_API = "https://my-bdd-server-nlngw9uco-jviejo.vercel.app"
+
+function getProductos() {
+  return axios.get(`${URL_API}/sql?sql=select * from Products order by Product_name`);
+}
+
+export default function ListaProductos() {
+  const { isLoading, data } = useQuery('products', getProductos)
+  
+  if (isLoading) {
+    return <div>Is loading...</div>
+  }
   return (
-    <div>Productos</div>
+    <div>
+      {JSON.stringify(data)}
+    </div>
   )
 }
