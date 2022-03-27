8c9912f7b3ab5c4127d1fec51f577ea47c1aed4c
Marceli Subirana <msubiw@gmail.com>
2022-03-27 18:57:36 +0200

    update

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import {QueryClient, QueryClientProvider } from 'react-query'
 import {Outlet} from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
 
+const queryClient = new QueryClient()
+
 export default function Home() {
   return (
-    <div className='d-flex flex-column container fullscreen'>
-      
-      <Header></Header>      
-      <main className='flex-grow-1'>
-        <Outlet />
-      </main>      
-      <Footer ></Footer>
-           
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='container d-flex flex-column container fullscreen'>
+        
+        <Header></Header>      
+        <main className='flex-grow-1'>
+          <Outlet />
+        </main>      
+        <Footer ></Footer>
+            
+      </div>
+    </QueryClientProvider>
   )
 }
